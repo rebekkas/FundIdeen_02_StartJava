4a27e47cd044a1349ca715ea1df009ee3862a99b
Paul Libbrecht <paul@hoplahup.net>
2013-11-14 12:50:33 +0100

    mit kleinen Syntaxbeispielen

--- src/main/java/phWGinfo/fundIdeen_02/DemoMethodenParameter.java
+++ src/main/java/phWGinfo/fundIdeen_02/DemoMethodenParameter.java
@@ -4,6 +4,7 @@ package phWGinfo.fundIdeen_02;
  */
 public class DemoMethodenParameter {
 
+    int count = 0;
     // Beispiel Methodenparameter
     // new DemoMethodenParameter().run() vom main Arufen!
 
@@ -11,13 +12,18 @@ public class DemoMethodenParameter {
         // Beispielmethode: sagWas
         sagWas("Hallo?");
         sagWas("Ist da jemand?");
-
+        System.out.println(count);
     }
 
     private void sagWas(String was) {
+        count = count +1;
         System.out.println("Sagt      : " + was);
         was = was.toLowerCase();
         System.out.println("Hat gesagt: " + was);
+    }
+
+    public int wieViel() {
+        return count;
     }
 
 
